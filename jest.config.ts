ca4598c93f74d9232452da09def2fe4f01b1a480
Samuel Richardson <sam@richardson.co.nz>
2025-09-05 13:35:37 +1000

    fix(jest): handle ESM format in Jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,10 @@
-module.exports = {
-  preset: "ts-jest",
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "jsdom",
+  globals: { "ts-jest": { useESM: true } },
+  extensionsToTreatAsEsm: [".ts"],
 };
+
+export default config;
