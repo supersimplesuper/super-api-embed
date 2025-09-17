a85973b57d61921bf9d94d72080e02fe9fff1f42
Nathan Yam <nathancyam@gmail.com>
2025-09-17 15:45:32 +1000

    fix: remove remote ID from MFA payload
    BREAKING CHANGE: Removes the remote ID from the MFA payload

--- src/services/messages/v1/mfa_verification_complete.ts
+++ src/services/messages/v1/mfa_verification_complete.ts
@@ -1,10 +1,18 @@
 import { MESSAGE_KIND } from "../../messages";
 
 export type Data = {
+  /**
+   * A ISO 8601 compliant date time string indicating when the MFA session was
+   * verified. This will be `null` when verifications attempts have been exceeded.
+   */
   verified_at: string | null;
-  remote_id: string | null;
 };
 
+/**
+ * A message type emitted when the user has either completed their MFA session or
+ * have exceeded the maximum number of attempts. Successful attempts will have their
+ * `verfied_at` property as an ISO-8601 string, while failed attempts will be `null`.
+ */
 export type Message = {
   data: Data;
   kind: MESSAGE_KIND.MFA_VERIFICATION_COMPLETED;
